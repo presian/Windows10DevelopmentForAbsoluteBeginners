ff48c10f74bf343fa1ec31bc9605ac11499846b8
Presian Danailov <presian@gbg.bg>
2017-01-09 00:30:04 +0200

    Add weather app

---
+++ WeatherApp/WeatherApp/Models/Clouds.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.Serialization;
+
+namespace WeatherApp.Models
+{
+    [DataContract(Name = "Clouds")]
+    public class Clouds
+    {
+        [DataMember(Name = "all")]
+        public int All { get; set; }
+    }
+}
---
+++ WeatherApp/WeatherApp/Models/Weather.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.Serialization;
+
+namespace WeatherApp.Models
+{
+    [DataContract]
+    public class Weather
+    {
+        [DataMember(Name = "id")]
+        public int Id { get; set; }
+
+        [DataMember(Name = "main")]
+        public string Main { get; set; }
+
+        [DataMember(Name = "description")]
+        public string Description { get; set; }
+
+        [DataMember(Name = "icon")]
+        public string Icon { get; set; }
+    }
+}
